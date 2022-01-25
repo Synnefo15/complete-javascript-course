74de7cf26ea3ef1cabbe249b3375e1c8ed99ce9b
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-01-25 14:15:36 +0700

    logical

---
+++ 01-Fundamentals-Part-1/challenges/chall.js
@@ -0,0 +1,75 @@
+// * Challenge 1,2
+
+/*
+let markMass1 = 78;
+let markHeight1 = 1.69;
+let johnMass1 = 92;
+let johnHeight1 = 1.95;
+
+let markMass2 = 95;
+let markHeight2 = 1.88;
+let johnMass2 = 85;
+let johnHeight2 = 1.76;
+
+
+let markBMI1 = markMass1/markHeight1**2;
+let johnBMI1 = johnMass1/johnHeight1**2;
+
+let markBMI2 = markMass2/markHeight2**2;
+let johnBMI2 = johnMass2/johnHeight2**2;
+
+let markHigherBMI=markBMI1 > johnBMI1;
+
+if (markBMI1 > johnBMI1) {
+    console.log(`Mark BMI = ${markBMI1} lebih tinggi dari John = ${johnBMI1} `);
+} else {
+    console.log(`John BMI = ${johnBMI1} lebih tinggi dari Mark = ${markBMI1} `);
+}
+*/
+
+// * Challenge 3
+
+console.log("No 1");
+dol = (96+108+89)/3;
+koa = (88+91+110)/3;
+console.log(`AVG DOL: ${dol} --- AVG koa: ${koa}`);
+
+console.log("No 2");
+if (dol == koa ) {
+    console.log("Draw");
+} else if(dol>koa) {
+    console.log("Dol Wins");
+}else{
+    console.log("Koa Wins");
+}
+
+console.log("No 3");
+dol1 = (97+112+101)/3;
+koa1 = (109+95+123)/3;
+console.log(`AVG DOL 1: ${dol1} --- AVG koa 1: ${koa1}`);
+if ((dol1 || koa1) >= 100 ) {
+    if (dol1>koa1) {
+        console.log('Yang menang dol1');
+    } else if (dol1<koa1) {
+        console.log('Yang menang koa1');
+    }
+} else {
+    console.log('Nilai harus minim 100');
+}
+
+console.log('NO 4');
+dol2 = (97+112+101)/3;
+koa2 = (109+95+106)/3;
+console.log(`AVG DOL 2: ${dol2} --- AVG koa 2: ${koa2}`);
+if (dol2 || koa2 >= 100) {
+    if (dol2 > koa2) {
+			console.log("Yang menang dol2");
+		}else if(dol2 === koa2){
+            console.log("Nilai Draw");
+        } 
+        else {
+			console.log("Yang menang koa2");
+		}
+} else {
+    console.log("Nilai tidak mencukupi 100");
+}
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -43,6 +43,7 @@ mantap`);
 */
 
 // * 18 If Else
+/*
 const age = 15;
 const cukupTua = age >= 18;
 
@@ -61,5 +62,73 @@ if (tahunLahir <= 2000) {
     abad = 21;
 }
 console.log(abad);
+*/
 
+// * 20 Conversion
+// & type conversion
+/*
+const inputYear = '2000';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear)+18);
 
+console.log(Number('Jonas'));
+console.log(String(23));
+
+// & type coercion : bisa ngubah type data
+console.log("I'm " + 23 + " Old");
+console.log("12"+"10"-2);
+
+console.log('9'-'5'); //4
+console.log('19'-'13'+'17'); //617
+console.log('19'-'13' + 17); //23
+console.log('123' < 57); //false
+console.log(5+6+'4'+9); //11 114 123 119 117
+*/
+
+// * 21 Truth & False value
+// false value = 0,'',undefined, null,NaN
+/*
+console.log(Boolean(undefined));
+
+const money = 1;
+if (money) {
+    console.log("Jangan Boros");
+}else{
+    console.log("Cari kerja");
+}
+*/
+
+// * 22 Equality Ops
+/*
+const age = '18';
+if (age == 18) console.log("Sudah 18");
+
+const fav = Number(prompt("Umur berapa? "));
+console.log(typeof(fav));
+
+if (fav === 21) {
+    console.log(`Umur anda 21 ya` );
+}else if( fav === 7){
+    console.log(`umur anda 7 kan`);
+}else{
+    console.log(`umur anda ${fav}`);
+}
+*/
+
+//* 24 Logical ops 
+const hasDriverLis = true;
+const hasGoodVis = true;
+
+console.log(hasDriverLis && hasGoodVis);
+console.log(hasDriverLis || hasGoodVis);
+console.log(!hasDriverLis || hasGoodVis);
+
+const shouldDrive = hasDriverLis && hasGoodVis;
+if (shouldDrive) {
+    console.log(`Boleh nyetir`);
+}else{
+    console.log(`Tidak boleh nyetir`);
+}
+
+const isOld = false;
+console.log((hasDriverLis || hasGoodVis) && isOld);
