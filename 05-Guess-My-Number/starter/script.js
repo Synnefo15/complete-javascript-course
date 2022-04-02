16d918bae36dec056a5fb53c92a438d88ee878ec
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-04-02 17:08:46 +0700

    guess num

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,43 +12,61 @@
 // document.querySelector('.guess').value = 23
 // console.log(document.querySelector('.guess').value);
 
-// *============ 73, 74  ============
-const secretNumber = Math.trunc(Math.random() * 20) + 1; // % merandom angka
+// *============ Project 1 guess number  ============
+let secretNumber = Math.trunc(Math.random() * 20) + 1; // % merandom angka
 let score = 20;
 document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+
+
+const displayMessage = function(message){
+	document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value);
 	console.log(guess, typeof guess);
 
-    // % Ketika no input 
+	// % Ketika no input
 	if (!guess) {
-		document.querySelector('.message').textContent = 'No Number!';
+		displayMessage('No Number!!!')
 		// % Ketika menang
 	} else if (guess === secretNumber) {
-		document.querySelector('.message').textContent = 'Angka anda benar !!!';
-        document.querySelector('body').style.backgroundColor = '#60b347';
+		
+		displayMessage('Angka anda benar !!!');
+		document.querySelector('body').style.backgroundColor = '#60b347';
 
-        document.querySelector('.number').style.width = '30rem'
-        // % ketinggian 
-	} else if (guess > secretNumber) {
+		document.querySelector('.number').style.width = '30rem';
+
+		if (score > highScore) {
+			highScore = score;
+			document.querySelector('.highscore').textContent = highScore;
+		}
+
+	} else if(guess !== secretNumber){
+		// % ketinggian / kerendahan
 		if (score > 1) {
-			document.querySelector('.message').textContent = 'ketinggian bro!!!';
+			displayMessage(guess > secretNumber ? 'ketinggian bro!!!' : 'terlalu kecil');
 			score--;
 			document.querySelector('.score').textContent = score;
 		} else {
-			document.querySelector('.message').textContent = 'Anda Kalah!!!';
+			displayMessage('Anda Kalah...')
 			document.querySelector('.score').textContent = 0;
 		}
-        // % kerendahan 
-	} else if (guess < secretNumber) {
-		if (score > 1) {
-			document.querySelector('.message').textContent = 'low bro!!!';
-			score--;
-			document.querySelector('.score').textContent = score;
-		} else {
-			document.querySelector('.message').textContent = 'Anda Kalah!!!';
-			document.querySelector('.score').textContent = 0;
-		}
-	}
+
+	} 
+	
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+	score = 20;
+	secretNumber = Math.trunc(Math.random() * 20) + 1; // % merandom angka
+
+	displayMessage('mulai tebak ...');
+	document.querySelector('.score').textContent = score;
+	document.querySelector('.number').textContent = '?';
+	document.querySelector('.guess').value = '';
+
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
 });
